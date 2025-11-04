4ba6648303d5ee70bd8202aa471cdb695812cd75
default <marko.skorupan@stu.ocsb.ca>
2025-11-04 23:13:38 +0000

    This program shows variable declarations

---
+++ main.ts
@@ -0,0 +1,34 @@
+/**
+ * @author Marko Skorupan
+ * @version 1.0.0
+ * @date 2025-11-04
+ * @fileoverview "This program shows variable declarations."
+ */
+
+// number data type count
+let count: number = 20;
+
+//number data type big number
+let big_number: number = 14500000000;
+
+// number data type temperature
+let temperature: number = -32.4;
+
+// number data type molecules
+let molecules: number = 158799625.35;
+
+//string data type
+let courseCode: string = "IC3SU";
+
+//boolean type play_again
+let play_again: boolean = true;
+
+//display each data type to the screen
+console.log ("Can you count up to " + count + "?")
+console.log("This is a really big number " + big_number)
+console.log("The outside temperature currently is " + temperature)
+console.log("A drop of eater might have " + temperature)
+console.log("Do you like " + courseCode + "?")
+console.log("True/False Computer programming is cool: " + play_again)
+
+console.log("\nDone")
